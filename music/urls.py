09d61961e3c9525afef5903f08142ce82b960196
Yashaswi4701 <yashaswisingh47@gmail.com>
2023-06-23 20:22:15 +0530

    first

---
+++ music/urls.py
@@ -0,0 +1,18 @@
+from django.urls import include, path
+
+from . import views
+
+
+app_name = 'music'
+
+urlpatterns = [
+	#/music/
+    path(r'^$', views.IndexView.as_view(), name='index'),
+
+    #/music/{album_id}/
+    path(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+
+    #/music/album/add
+    path(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+
+]
